aae2aab489c3b7700ab1eecf48f862a3f7b9f912
hiderval <57116359+hiderval@users.noreply.github.com>
2019-12-10 21:07:56 -0300

    Projeto react
    projeto aula 2

--- app.js
+++ app.js
@@ -7,3 +7,5 @@ const module2 = require('./mod2')
 
 module1('Hiderval Teste')
 module2.mensagem('Teste Modulo 2')
+
+C:\ProgramData\Oracle\Java\javapath;%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\;C:\Program Files\dotnet\;C:\Program Files (x86)\MySQL\MySQL Utilities 1.6\;C:\Program Files\Microsoft SQL Server\130\Tools\Binn\;C:\Program Files (x86)\Microsoft SQL Server\120\DTS\Binn\;C:\Program Files (x86)\Microsoft SQL Server\Client SDK\ODBC\110\Tools\Binn\;C:\Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\;C:\Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\;C:\Program Files\Git\cmd;C:\Program Files\Microsoft VS Code\bin;C:\Program Files\nodejs\
